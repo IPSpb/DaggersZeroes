dd4180eadedd920888158a45924102fc5004fad7
Igor <warrior84spb@gmail.com>
2014-04-25 13:09:29 +0400

    Create class Field

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        Field field = new Field();
+        field.eraseField();
+        field.showField();
+
     }
 }
